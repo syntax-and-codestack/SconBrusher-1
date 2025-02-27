b27945f23796df79226d7a3e3723b7e653cf43d6
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-02-27 16:52:14 +0000

    git -o

---
+++ source/brush.py
@@ -0,0 +1,19 @@
+import Math.py as math
+import string
+
+#brush is a 3d polygon that can be modifed on sides
+
+class brush_t():
+
+    p_yBrushId = int
+    Drag_Brush = bool
+
+MemBrush = brush_t & brush_t *next(brush_t)
+
+MAX_BRUSH_SIZE = int
+
+
+
+    
+
+    
